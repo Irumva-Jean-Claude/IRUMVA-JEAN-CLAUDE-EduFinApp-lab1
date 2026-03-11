156efe9aa8563d68a6b9f1e95926f887b20f765d
irumvajeanclaude <irumvajeanclaude87@gmail.com>
2026-03-11 14:09:02 +0200

    step 8

--- EduFinApp/core/models.py
+++ EduFinApp/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Testing(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    DateTimeField=models.TextField(auto_now_add=True)
+    DateTimeField = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
